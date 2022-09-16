723ebf26db412fff6723b1653d635b4161e02ad4
manfred nacktname <mansurgewinner@gmail.com>
2022-09-16 18:37:23 +0200

    printPrimes() Methode fertig

--- Haus-bung1/src/EratosthenesPrimeSieve.java
+++ Haus-bung1/src/EratosthenesPrimeSieve.java
@@ -1,9 +1,15 @@
 public class EratosthenesPrimeSieve implements PrimeSieve{
+    private int obergrenze = 0;
+
+    public EratosthenesPrimeSieve(int obergrenze) {
+        this.obergrenze = obergrenze;
+    }
+
     @Override
     public boolean isPrime(int p) {
-        for (int i = p - 1; i > 1;i--)
+        for (int i = p - 1; i > 1;i--)  //i geht durch alle Zahlen durch die < p sind
         {
-            if (p%i == 0.0) {
+            if (p%i == 0.0) {   //Kontrolle, ob eins davon p teilen kann
                 return false;
             }
         }
@@ -12,6 +18,30 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
 
     @Override
     public void printPrimes() {
+        if (obergrenze == 0)    //Kontrolle
+        {System.out.println("obergrenze muss größer als 0 sein");
+         return;}
 
+        boolean[] gestrichen = new boolean[obergrenze]; //unmarkierte zahlen gespeichert
+        for (int i = 2; i < obergrenze; i++) {
+            gestrichen[i] = false;
+        }
+
+        for (int basis = 2; basis*2 < obergrenze; basis++) {     //nicht primzahlen markieren
+            if (gestrichen[basis] == false)
+            {
+                for (int mal = 2; basis*mal < obergrenze; mal++)
+                {
+                    gestrichen[basis*mal] = true;
+                }
+            }
+        }
+
+        for (int i = 2; i < obergrenze; i++) {
+            if (gestrichen[i] == false)
+            {
+            System.out.println(i);
+            }
+        }
     }
 }
--- Haus-bung1/src/Main.java
+++ Haus-bung1/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        //EratosthenesPrimeSieve e = new EratosthenesPrimeSieve();  //zum Testen
-        //System.out.println(e.isPrime(212));
+        EratosthenesPrimeSieve e = new EratosthenesPrimeSieve(20);  //zum Testen
+        System.out.println(e.isPrime(10));
+        System.out.println("");
+        e.printPrimes();
 
     }
 }
